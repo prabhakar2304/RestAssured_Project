f052d9d22eed7f401e0727765c18a318c9228bcf
Prabhakar <Prabhakarsingh23041@gmail.com>
2026-07-21 21:40:07 +0530

    new changes

--- src/test/java/DAY_4/ParsingJSONResponseData.java
+++ src/test/java/DAY_4/ParsingJSONResponseData.java
@@ -1,8 +1,14 @@
 package DAY_4;
 
 import static io.restassured.RestAssured.*;
+
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.*;
+import org.json.JSONArray;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import java.util.HashMap;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 
@@ -20,8 +26,37 @@ public class ParsingJSONResponseData
 	 .then()
 	        .statusCode(200)
 	        .header("Content-Type", "application/json")
-	        .body("[10].name", equalTo("rajesh"));
+	        .body("[0].name", equalTo("arun"));
 	}
-	
+    //Approach 2
+	@Test(priority=2)
+	void testJSONResponse2()
+	{
+	Response response=given()
+			           .contentType("Content-Type.JSON")
+			.when()
+			           .get("http://localhost:3000/students");
+	     
+	         Assert.assertEquals(response.getStatusCode(), 200);
+	         Assert.assertEquals(response.getHeader("Content-Type"), "application/json");
+	         String nameString= response.jsonPath().get("[0].name").toString();
+	         Assert.assertEquals(nameString, "arun");
+	        }
+	@Test(priority=3)
+	 void testJSONOBJECTResponse3()
+	 {
+		Response response=given()
+			           .contentType(ContentType.JSON)
+			.when()
+			           .get("http://localhost:3000/students");
+	     
+	    JSONArray jarr = new JSONArray(response.asString());
 
+	    for (int i = 0; i < jarr.length(); i++) {
+
+	        String studentName = jarr.getJSONObject(i).getString("name");
+	        System.out.println("Student Name is: " + studentName);
+	         
+	 }
+	 }
 }
